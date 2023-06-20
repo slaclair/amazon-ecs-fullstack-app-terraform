2b610a3398c6591f8a16514150027e24ea0d6f95
slaclair <87423034+slaclair@users.noreply.github.com>
2023-06-20 16:46:54 -0400

    Update main.tf
    S3 bucket ACL's

--- Infrastructure/Modules/S3/main.tf
+++ Infrastructure/Modules/S3/main.tf
@@ -13,3 +13,16 @@ resource "aws_s3_bucket" "s3_bucket" {
     Name = var.bucket_name
   }
 }
+resource "aws_s3_bucket_ownership_controls" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket]
+
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
+}
